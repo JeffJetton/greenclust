9f5f2a5f97a9aff28b70d715730d90a1108620e4
Jeff Jetton <jeff.jetton@gmail.com>
2019-03-23 11:56:47 -0500

    Add missing category handling code and update docs

--- R/assigncluster.R
+++ R/assigncluster.R
@@ -14,46 +14,52 @@
 #' Assign clusters to a new vector of categories
 #'
 #' Maps a vector of cluster numbers to another categorical vector,
-#' yielding a new vector of matching cluster numbers.
+#' yielding a new vector of matching cluster numbers. Useful for distributing
+#' cluster numbers back out to the original observations in cases where the
+#' clustering was performed on a table of unique levels rather than directly
+#' on the observations (such as with \code{\link{greenclust}}).
 #'
-#' Useful for cases where clustering was performed on a table of unique levels
-#' (as with \code{\link{greenclust}}), and the clusters need to be distributed
-#' back out to the original observations. In this case, the \code{cluster}
-#' vector will typically have a \code{names} attribute of corresponding
-#' categories, and \code{src} will not be used.
+#' Any categories in \code{x} that do not exist as names in \code{cluster}
+#' are assigned the cluster number that is most-frequently assigned to the
+#' other, existing categories (with ties going to the lowest cluster number).
 #'
-#' Also handy for assigning clusters to a test data set when the original
-#' clustering was done on a training set. Here, the names of \code{cluster}
-#' will represent original observations rather than category names, so the
-#' \code{src} vector will be needed.
-#'
-#' Any categories in \code{x} that do not exist as either names in
-#' \code{cluster} or elements in \code{src} are assigned the most frequent
-#' cluster number (with ties going to the lowest value).
+#' If there are duplicate names in \code{cluster}, the first occurrence
+#' takes precedence.
 #' @param x a factor or character vector representing a categorical variable
 #' @param clusters a numeric vector representing cluster numbers, such as an
-#'   object returned by \code{\link{greenclust}} or \code{\link{hclust}}
-#' @param src optional factor or character vector of categories to use for
-#'   the cluster numbers in \code{cluster} if they do not already exist in
-#'   the  \code{names} attribute of  \code{cluster}
+#'   object returned by \code{\link{greencut}} or \code{\link{cutree}}
+#' @return A numeric vector of the same length as \code{x}, representing
+#'   assigned cluster numbers.
 #' @examples
-#' # Example goes here
-#'
+#' # Cluster feed types based on number of "underweight" chicks
+#' grc <- greenclust(table(chickwts$feed,
+#'                         ifelse(chickwts$weight < 200, "Y", "N")))
+#' # Assign clusters to each original observation
+#' feed.clustered <- assign.cluster(chickwts$feed, greencut(grc))
+#' table(chickwts$feed, feed.clustered)
 #' @export
 assign.cluster <- function(x, clusters) {
-    # TODO:   Check for unique cluster names
-    #         Handle cases where x category not in clusters
-    #         At documentation above discussing these two features ^^^
-    #         Accept an optional vector of source observations to
-    #         get levels from (in cases where the train set was hclusted)
-    #
-    #              If categories are not unique, the most-frequently occuring cluster
-    #              number for each category is mapped (minimum if ties).
-    #            If a category in x is not found in clusters, it is assigned the most-popular
-    #            cluster assigned by the existing categories (minimum if ties)
-    if (length(unique(names(clusters))) != length(clusters)) {
-        # Determine which cluster to use for each unique category
+    # TODO:   Handle cases where x category not in clusters
 
+    # Check for NA, and other invalid types
+
+    # Note that match() will, by default, return the first match only
+    newx <- clusters[match(x, names(clusters))]
+
+    # Any categories in x that weren't names in clusters?
+    if (anyNA(newx)) {
+        # Get most-frequently assigned cluster so far
+        tab <- table(newx)
+        most.freq <- tab[order(as.numeric(tab), as.numeric(names(tab)),
+                         decreasing=c(TRUE, FALSE))][1]
+        # Assign that cluster to all the NAs
+        na.vector <- is.na(newx)
+        newx[na.vector] <- as.numeric(names(most.freq))
+        names(newx)[na.vector] <- x[na.vector]
     }
-    return(as.factor(clusters[match(x, names(clusters))]))
+
+    return(newx)
 }
+
+
+

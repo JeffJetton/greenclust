be007d0abd64ed36d6e607aad0682913d6824d23
Jeff Jetton <jeff.jetton@gmail.com>
2019-06-25 06:54:07 -0500

    Use more realistic (coercible to numeric) matrix row names in tests

--- tests/testthat/test-greenclust-hidden.R
+++ tests/testthat/test-greenclust-hidden.R
@@ -4,27 +4,28 @@ library(greenclust)
 # Test matrix
 m <- matrix(1:16, ncol=4)
 colnames(m) <- paste0("Col", 1:4)
-rownames(m) <- c("Alpha", "Bravo", "Charlie", "Delta")
+rownames(m) <- 1:nrow(m)
 
 
 test_that(".combine.rows returns a matrix with one fewer row", {
-    expect_equal(nrow(greenclust:::.combine.rows(m, 1, 2, "Echo")), nrow(m) - 1)
+    expect_equal(nrow(greenclust:::.combine.rows(m, 1, 2, 42)), nrow(m) - 1)
 })
 
 test_that(".combine.rows returns a matrix with same number of equally-named columns", {
-    expect_equal(colnames(greenclust:::.combine.rows(m, 3, 4, "Foxtrot")), colnames(m))
+    expect_equal(colnames(greenclust:::.combine.rows(m, 3, 4, 99)), colnames(m))
 })
 
 test_that(".combine.rows returns a matrix with a correctly-combined row", {
-    combo <- greenclust:::.combine.rows(m, 2, 4, "Golf")
+    combo <- greenclust:::.combine.rows(m, 2, 4, 86)
     expect_equal(sum(combo), sum(m))
-    expect_equal(combo["Golf",], apply(m[c(2, 4), ], 2, sum))
+    expect_equal(combo["86",], apply(m[c(2, 4), ], 2, sum))
 })
 
 test_that(".print.step returns correct cluster step information", {
+    # Set one row name to a negative (representing unclustered)
     rownames(m)[3] <- -2
     expect_output(greenclust:::.print.step(m, c("Whiskey", "X-Ray", "Yankee", "Zulu"),
-                                  99,TRUE, 0.424242, 0.8675309, 2.0),
+                                           99,TRUE, 0.424242, 0.8675309, 2.0),
                   "^Step.*99.*tie")
     expect_output(greenclust:::.print.step(m, c("Whiskey", "X-Ray", "Yankee", "Zulu"),
                                            99,TRUE, 0.424242, 0.8675309, 2.0),
